11d8cea4093d3f074fe5f7669bc7be3b1ab05794
Edward Chen <echen333us@gmail.com>
2022-09-04 18:29:53 -0400

    leaflet working

--- frontend/src/components/Demo.js
+++ frontend/src/components/Demo.js
@@ -1,16 +1,32 @@
-function Demo() {
-    return (
-        <div className="DemoAll">
-            <h1>Forest Watch Demo!</h1>
-            <div>
-        
-            <p> Using machine learning to detect forest fires before they happen.</p>
-        
-                <p> Check out our <a href="/demo">Demo</a>! </p>
-            </div>
+import { MapContainer, TileLayer, useMap, Popup, Marker } from 'react-leaflet'
+import React, { useEffect } from 'react';
+import MarkerClusterGroup from "react-leaflet-markercluster";
 
+import "../styles.scss";
+
+export default function Demo() {
+
+    return (
+        <div>
+            <h2> Hi</h2>
+            <MapContainer
+            className="markercluster-map"
+            center={[51.0, 19.0]}
+            zoom={4}
+            maxZoom={18}
+            >
+                <TileLayer
+                    url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png"
+                    attribution='&copy; <a href="http://osm.org/copyright">OpenStreetMap</a> contributors'
+                />
+
+                    <Marker position={[49.8397, 24.0297]} />
+                {/* <MarkerClusterGroup> */}
+                    <Marker position={[52.2297, 21.0122]} />
+                    {/* <Marker position={[51.5074, -0.0901]} /> */}
+                {/* </MarkerClusterGroup> */}
+            </MapContainer>
         </div>
     );
   }
   
-  export default Demo; 
